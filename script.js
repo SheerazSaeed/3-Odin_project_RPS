fdd1f6fd4ac15b4178f126f93b3c5c3a17060ac8
sheeraz <sheerazsaeed22@hotmail.co.uk>
2023-12-25 14:32:04 +0000

    UI updated. Registers score for both sides up to 5

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+let playerScore = 0;
+let computerScore = 0;
+
 document.getElementById('rock').addEventListener('click', () => playGame('rock'));
 document.getElementById('paper').addEventListener('click', () => playGame('paper'));
 document.getElementById('scissors').addEventListener('click', () => playGame('scissors'));
 
 function playGame(playerMove) {
+  if(playerScore >= 5 || computerScore >= 5) {
+    updateResult("Game over. Please refresh the page to start again.");
+    return;
+  }
+
   const computerMove = pickComputerMove();
   const result = determineWinner(playerMove, computerMove);
-  alert(`You picked ${playerMove}. Computer picked ${computerMove}. ${result}`);
+
+  updateScore(result);
+  updateResult(`You picked ${playerMove}. Computer picked ${computerMove}. ${result}`);
+  checkForWinner();
 }
 
 function pickComputerMove() {
@@ -22,8 +33,26 @@ function determineWinner(playerMove, computerMove) {
   if ((playerMove === 'rock' && computerMove === 'scissors') ||
       (playerMove === 'scissors' && computerMove === 'paper') ||
       (playerMove === 'paper' && computerMove === 'rock')) {
+    playerScore++;
     return 'You win.';
+  } else {
+    computerScore++;
+    return 'You lose.';
   }
+}
 
-  return 'You lose.';
+function updateResult(text) {
+  document.getElementById('result').textContent = text;
+}
+
+function updateScore(result) {
+  document.getElementById('score').textContent = `Your Score: ${playerScore} | Computer Score: ${computerScore}`;
+}
+
+function checkForWinner() {
+  if (playerScore === 5) {
+    updateResult("Congratulations! You've won the game!");
+  } else if (computerScore === 5) {
+    updateResult("You lose.");
+  }
 }
